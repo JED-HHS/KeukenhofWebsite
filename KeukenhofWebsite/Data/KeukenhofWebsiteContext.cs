42e96f904a8c95eb7a0f025c088e0eb72987923a
daanbom <daan_bom@live.nl>
2020-01-09 14:31:15 +0100

    Homepagina inclusief plaatje

---
+++ KeukenhofWebsite/Data/KeukenhofWebsiteContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace KeukenhofWebsite.Models
+{
+    public class KeukenhofWebsiteContext : DbContext
+    {
+        public KeukenhofWebsiteContext (DbContextOptions<KeukenhofWebsiteContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<KeukenhofWebsite.Models.test> test { get; set; }
+    }
+}
