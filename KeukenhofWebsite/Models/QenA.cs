862030a39928c6428b33fba4f459f2bc37db20ec
jordyvandorp <49026613+jordyvandorp@users.noreply.github.com>
2020-01-15 15:20:48 +0100

    Azure database migration

--- KeukenhofWebsite/Models/QenA.cs
+++ KeukenhofWebsite/Models/QenA.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,7 @@ namespace KeukenhofWebsite.Models
 {
     public class QenA
     {
-        public int AnswerId { get; set; }
+        [Key] public int AnswerId { get; set; }
         public string Question { get; set; }
         public string Answer { get; set; }
     }
