4144776b3e1b15034ea9d02981fe2a0672920c46
jordy <jordydorp@gmail.com>
2020-01-15 14:30:22 +0100

    Model classes

---
+++ KeukenhofWebsite/Models/Park.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KeukenhofWebsite.Models
+{
+    public class Park
+    {
+        public string Naam { get; set; }
+        public DateTime Openingsdag { get; set; }
+        public DateTime Sluitingsdag { get; set; }
+        public DateTime OpeningstijdenMaandag { get; set; }
+        public DateTime OpeningstijdenDinsdag { get; set; }
+        public DateTime OpeningstijdenWoensdag { get; set; }
+        public DateTime OpeningstijdenDonderdag { get; set; }
+        public DateTime OpeningstijdenVrijdag { get; set; }
+        public DateTime OpeningstijdenZaterdag { get; set; }
+        public DateTime OpeningstijdenZondag { get; set; }
+    }
+}
---
+++ KeukenhofWebsite/Models/QenA.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KeukenhofWebsite.Models
+{
+    public class QenA
+    {
+        public int AnswerId { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
+    }
+}
