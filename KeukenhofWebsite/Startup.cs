a5cde4e2e1bd5e8413b4b54a48d9961bcd86221f
Tim Batist <timbatist1999@hotmail.nl>
2020-01-17 12:10:59 +0100

    Provacy Statement gemaakt

--- KeukenhofWebsite/Startup.cs
+++ KeukenhofWebsite/Startup.cs
@@ -61,7 +61,7 @@ namespace KeukenhofWebsite
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Bereikbaarheid}/{id?}");
+                    template: "{controller=Home}/{action=Privacy_statement}/{id?}");
             });
         }
     }
