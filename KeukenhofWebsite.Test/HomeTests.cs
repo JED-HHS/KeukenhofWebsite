8d93484f6defe38cc42a7dd0184a00df7b1b1512
daanbom <daan_bom@live.nl>
2020-01-23 12:34:43 +0100

    homecontroller tests (werken nog niet)

--- KeukenhofWebsite.Test/HomeTests.cs
+++ KeukenhofWebsite.Test/HomeTests.cs
@@ -13,21 +13,57 @@ namespace KeukenhofWebsite.Test
 {
     public class HomeTests
     {
+
+        private readonly KeukenhofWebsiteContext _context;
+
+
+        [Theory]
+        [InlineData("/")]
+        [InlineData("/HetPark")]
+        [InlineData("/Content")]
+        [InlineData("/FAQ")]
+        [InlineData("/_Contact")]
+        public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
+        {
+            HomeController c = new HomeController();
+            //var client = c.C
+        }
+
         [Fact]
         public void HetParkTesten()
         {
             HomeController c = new HomeController();
-            ViewResult result = c.HetPark() as ViewResult;
-            Assert.NotNull(result);
+            var result = c.HetPark() as ViewResult;
+            var obj = new ViewResult();
+            var redirectToActionResult = Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("Home", redirectToActionResult.ControllerName);
+            Assert.Equal("HetPark", redirectToActionResult.ActionName);
+            //var ViewResult = Assert.IsType<ViewResult>(result);
+            //var redirectToActionResult = Assert.IsType<RedirectToActionResult>(result);
+            //Assert.Equal(obj.GetType(), result.GetType());
+           
+
+         
         }
 
         [Fact]
         public void FAQTesten()
         {
             HomeController c = new HomeController();
-            ViewResult result = c.FAQ() as ViewResult;
+            var result = c.FAQ() as ViewResult;
+            var myModel = (IEnumerable<KeukenhofWebsite.Models.QenA>)result.Model;
+            //var viewResult = Assert.IsType<ViewResult>(result);
+            //var model = viewResult.ViewData.Model;
             Assert.NotNull(result);
+            Assert.NotNull(result.Model); // add additional checks on the Model
+            Assert.True(string.IsNullOrEmpty(result.ViewName) || result.ViewName == "FAQ");
         }
+
+        /*var obj = new ViewResult();
+        var redirectToActionResult = Assert.IsType<RedirectToActionResult>(result);
+        Assert.Equal("Home", redirectToActionResult.ControllerName);
+        Assert.Equal("FAQ", redirectToActionResult.ActionName);*/
+
 
         [Fact]
         public void EvenementenTesten()
@@ -40,7 +76,7 @@ namespace KeukenhofWebsite.Test
         public void ContentTesten()
         {
             HomeController c = new HomeController();
-            ViewResult result = c.Content() as ViewResult;
+            var result = c.Content() as ViewResult;
             Assert.NotNull(result);
         }
 
@@ -56,8 +92,11 @@ namespace KeukenhofWebsite.Test
         public void ContactTesten()
         {
             HomeController c = new HomeController();
-            ViewResult result = c._Contact() as ViewResult;
-            Assert.NotNull(result);
+            var result = c._Contact() as ViewResult;
+           //var myModel = (_context.QenA.ToList())result.Model;
+            //var viewResult = Assert.IsType<ViewResult>(result);
+            //var model = viewResult.ViewData.Model;
+            Assert.Null(result);
         }
 
         /*[Fact]
